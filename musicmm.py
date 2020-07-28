4de5c56d9fd3cdac54ad9c1a2785bf5d891c90f8
Robin Scheibler <robin.scheibler@linecorp.com>
2020-07-28 11:46:03 +0900

    Adds draft of MMUSIC, not functional yet

---
+++ musicmm.py
@@ -0,0 +1,436 @@
+from enum import Enum
+
+import numpy as np
+
+import pyroomacoustics as pra
+from external_mdsbf import MDSBF
+from unit_ls import unit_ls
+from utils import geom
+
+
+def power_mean(X, s=1.0, *args, **kwargs):
+
+    if s != 1.0:
+        return (np.mean(X ** s, *args, **kwargs)) ** (1.0 / s)
+    else:
+        return np.mean(X, *args, **kwargs)
+
+
+def mmusic_cost(q, mics, wavenumbers, data, s=1.0):
+    """
+    Parameters
+    ----------
+    q: array_like, shape (n_dim)
+        the current propagation vector estimate
+    mics: ndarray, shape (n_mics, n_dim)
+        the location of the microphones
+    wavenumbers: ndarray, shape (n_freq)
+        the wavenumbers corresponding to each data bin
+    data: ndarray, shape (n_freq, n_mics)
+        the data that defines the noise subspace
+    s: float
+        the power mean parameter
+
+    Returns
+    -------
+    cost: float
+        the value of the MMUSIC cost function
+    """
+    n_mics, n_dim = mics.shape
+    n_freq, _ = data.shape
+
+    weights = np.zeros_like(data)
+
+    delta_t = mics @ q  # shape (n_mics)
+    e = wavenumbers[:, None] @ delta_t[None, :]  # shape (n_freq, n_mics)
+
+    E = np.concatenate((np.cos(e), np.sin(e)), axis=-1)  # shape (n_freq, 2 * n_mics)
+    ell = n_mics + 2 * np.sum(data * E, axis=-1)
+
+    cost = power_mean(ell, s=s)
+
+    return cost
+
+
+def mmusic_auxiliary_variables(q, mics, wavenumbers, data, s=1.0):
+    """
+    Parameters
+    ----------
+    q: array_like, shape (n_dim)
+        the current propagation vector estimate
+    mics: ndarray, shape (n_mics, n_dim)
+        the location of the microphones
+    wavenumbers: ndarray, shape (n_freq)
+        the wavenumbers corresponding to each data bin
+    data: ndarray, shape (n_freq, n_mics)
+        the data that defines the noise subspace
+    s: float
+        the power mean parameter
+
+    Returns
+    -------
+    new_data: ndarray, shape (n_points, n_mics)
+        the auxiliary right hand side
+    weights: ndarray, shape (n_points, n_mics)
+        the new weights
+    """
+    n_mics, n_dim = mics.shape
+    n_freq, _ = data.shape
+
+    weights = np.zeros_like(data)
+
+    delta_t = mics @ q  # shape (n_mics)
+    e = wavenumbers[:, None] @ delta_t[None, :]  # shape (n_freq, n_mics)
+
+    # compute the offset to pi
+    z_cos = np.round((np.pi - e) / (2 * np.pi))
+    new_data_cos = (1 - 2 * z_cos) * np.pi
+    phi_cos = new_data_cos - e
+
+    # compute the offset to pi
+    z_sin = np.round((0.5 * np.pi + e) / (2 * np.pi))
+    new_data_sin = (0.5 - 2 * z_sin) * np.pi
+    phi_sin = new_data_sin + e
+
+    # stick together the two sides
+    new_data = np.concatenate((-new_data_cos, -new_data_sin), axis=-1)
+    phi = np.concatenate((phi_cos, phi_sin), axis=-1)
+
+    # adjust right-hand side
+
+    # compute the weights
+    weights[:] = np.sinc(phi / np.pi) * data
+
+    # this the time-frequency bin weight corresponding to the robustifying function
+    # shape (n_points)
+    if s < 1.0:
+        E = np.concatenate((np.cos(e), np.sin(e)), axis=-1)
+        ell = n_mics + 2 * np.sum(data * E, axis=-1)
+        r = (1.0 / n_freq) * ell ** (s - 1.0) / np.mean(ell ** s) ** (1.0 - 1.0 / s)
+        weights *= r[:, None]
+
+    return new_data, weights
+
+
+class MMUSIC(pra.doa.MUSIC):
+    """
+    Implements the MUSCI DOA algorithm with optimization directly on the array
+    manifold using an MM algorithm
+
+    .. note:: Run locate_sources() to apply MMUSIC
+
+    Parameters
+    ----------
+    L: array_like, shape (n_dim, n_mics)
+        Contains the locations of the microphones in the columns
+    fs: int or float
+        Sampling frequency
+    nfft: int
+        FFT size
+    c: float, optional
+        Speed of sound
+    num_src: int, optional
+        The number of sources to recover (default 1)
+    s: float
+        The exponent for the robustifying function, we expect that making beta larger
+        should make the method less sensitive to outliers/noise
+    n_grid: int
+        The size of the grid search for initialization
+    track_cost: bool
+        If True, the cost function will be recorded
+    init_grid: int
+        Size of the grid for the rough initialization method
+    verbose: bool, optional
+        Whether to output intermediate result for debugging purposes
+    """
+
+    def __init__(
+        self,
+        L,
+        fs,
+        nfft,
+        c=343.0,
+        num_src=1,
+        s=1.0,
+        mode="far",
+        dim=None,
+        n_iter=30,
+        n_grid=100,
+        track_cost=False,
+        verbose=False,
+        *args,
+        **kwargs,
+    ):
+        """
+        The init method
+        """
+        self.s = s
+        self.n_iter = n_iter
+        self._track_cost = track_cost
+        self.verbose = verbose
+
+        L = np.array(L)
+
+        assert (
+            self.s <= 1.0
+        ), "The parameter s of MMUSIC should be smaller or equal to 1."
+
+        if dim is None:
+            dim = L.shape[0]
+
+        super().__init__(
+            L, fs, nfft, c=c, num_src=num_src, dim=dim, n_grid=n_grid, *args, **kwargs,
+        )
+
+        # differential microphone locations (for x-corr measurements)
+        # shape (n_dim, n_mics * (n_mics - 1) / 2)
+        self._L_diff = self._extract_off_diagonal(
+            self.L[:, :, None] - self.L[:, None, :]
+        )
+
+    def _extract_off_diagonal(self, X):
+        """
+        Parameters
+        ----------
+        X: array_like, shape (..., M, M)
+            A multi dimensional array
+
+        Returns
+        -------
+        Y: array_like, shape (..., M * (M - 1) / 2)
+            The linearized entries under the main diagonal
+        """
+        # we need to format the sensors
+        M = X.shape[-1]
+        assert X.shape[-2] == M
+        indices = np.arange(M)
+
+        mask = np.ravel_multi_index(
+            np.where(indices[:, None] > indices[None, :]), (M, M)
+        )
+
+        return X.reshape(X.shape[:-2] + (X.shape[-2] * X.shape[-1],))[..., mask]
+
+    def _cost(self, q, mics, wavenumbers, data):
+        """ Compute the cost of the function """
+        return mmusic_cost(q, mics, wavenumbers, data, s=self.s)
+
+    def _optimize_direction(
+        self, q, mics, wavenumbers, data, n_iter=1,
+    ):
+        """
+        Parameters
+        ----------
+        q: array_like, shape (n_dim,)
+            the initial direction vector
+        mics: ndarray, shape (n_mics, n_dim)
+            the location of the microphones
+        wavenumbers: ndarray, shape (n_freq)
+            the wavenumbers corresponding to each data bin
+        data: ndarray, shape (n_freq, n_mics)
+            the phase of the measurements
+        """
+
+        n_mics, n_dim = mics.shape
+        n_freq, _ = data.shape
+        assert wavenumbers.shape[0] == n_freq
+
+        # buffers
+        qs = np.zeros((1, n_dim), dtype=q.dtype)
+        qs[0, :] = q
+        weights = np.zeros((1, n_mics), dtype=data.dtype)
+        rhs = np.zeros((1, n_mics), dtype=data.dtype)
+
+        # broadcast the microphone matrix to the same size as weights and RHS
+        mics_bc = np.broadcast_to(mics, (1, n_mics, n_dim))
+
+        if self.verbose:
+            doa, r = geom.cartesian_to_spherical(qs.T)
+            print(f"Initialization")
+            print(
+                f"  colatitude={np.degrees(doa[0, :])}\n"
+                f"  azimuth=   {np.degrees(doa[1, :])}\n"
+            )
+
+        if self._track_cost:
+            self.cost.append([])
+            c = self._cost(qs[0], mics, wavenumbers, data)
+            self.cost[-1].append(c)
+            if self.verbose:
+                print(f"  cost: {c}")
+
+        for epoch in range(n_iter):
+
+            new_data, new_weights = mmusic_auxiliary_variables(
+                qs[0], mics, wavenumbers, data, s=self.s
+            )
+
+            w_red = np.sum(new_weights * wavenumbers[:, None] ** 2, axis=0)
+            weights[0, :] = w_red[:n_mics] + w_red[n_mics:]
+
+            r_red = np.sum(new_data * new_weights * wavenumbers[:, None], axis=0)
+            rhs[0, :] = (r_red[:n_mics] + r_red[n_mics:]) / weights[0, :]
+
+            qs[:] = unit_ls(mics_bc, rhs, weights=weights, tol=1e-8, max_iter=1000)
+
+            if self.verbose:
+                doa, r = geom.cartesian_to_spherical(qs.T)
+                print(f"Epoch {epoch}")
+                print(
+                    f"  colatitude={np.degrees(doa[0, :])}\n"
+                    f"  azimuth=   {np.degrees(doa[1, :])}\n"
+                )
+
+            if self._track_cost:
+                c = self._cost(qs[0], mics, wavenumbers, data)
+                self.cost[-1].append(c)
+                if self.verbose:
+                    print(f"  cost: {c}")
+
+        return qs[0], epoch
+
+    def _process(self, X):
+        """
+        Process the input data and computes the DOAs
+
+        Parameters
+        ----------
+        X: array_like, shape (n_channels, n_frequency, n_frames)
+            The multichannel STFT of the microphone signals
+            Set of signals in the frequency (RFFT) domain for current 
+            frame. Size should be M x F x S, where M should correspond to the 
+            number of microphones, F to nfft/2+1, and S to the number of snapshots 
+            (user-defined). It is recommended to have S >> M.
+        """
+        n_dim = self.dim
+        n_freq = len(self.freq_bins)
+        n_frames = X.shape[2]
+
+        assert self.L.shape[1] == X.shape[0]
+
+        # STEP 1: Classic MUSIC
+
+        # compute the covariance matrices
+        self.Pssl = np.zeros((self.num_freq, self.grid.n_points))
+        C_hat = self._compute_correlation_matricesvec(X)
+
+        # subspace decomposition (we need these for STEP 2)
+        Es, En, ws, wn = self._subspace_decomposition(C_hat)
+
+        # the mode vectors, shape (n_grid, n_freq, n_mics)
+        mod_vec = np.transpose(
+            np.array(self.mode_vec[self.freq_bins, :, :]), axes=[2, 0, 1]
+        )
+        self.Pssl = 1.0 / np.linalg.norm(
+            np.conj(mod_vec[:, :, None, :]) @ En[None, :, :], axis=(-1, -2)
+        )
+        self.grid.set_values(np.mean(self.Pssl, axis=-1))
+
+        # find the peaks for the initial estimate
+        self.src_idx = self.grid.find_peaks(k=self.num_src)
+        qs = self.grid.cartesian[:, self.src_idx].T
+
+        # STEP 2: refinement via the MM algorithm
+
+        # the wavenumbers (n_freq * n_frames)
+        wavenumbers = 2 * np.pi * self.freq_hz / self.c
+
+        # For x-corr measurements, we consider differences of microphones as sensors
+        # n_mics = n_channels * (n_channels - 1) / 2
+        n_mics = self._L_diff.shape[1]
+
+        # shape (n_mics, n_dim)
+        mics = self._L_diff.T
+
+        # shape (n_freq, n_mics)
+        v_c = self._extract_off_diagonal(En @ np.conj(En).swapaxes(-2, -1))
+        data = np.concatenate((np.real(v_c), np.imag(v_c)), axis=-1)
+
+        # Run the DOA algoritm
+        self.cost = []
+        for k, q in enumerate(qs):
+            qs[k, :], epochs = self._optimize_direction(
+                q, mics, wavenumbers, data, n_iter=self.n_iter
+            )
+
+        # Now we need to convert to azimuth/doa
+        # self._doa_recon, _ = geom.cartesian_to_spherical(qs.T)
+        self._doa_recon, _ = geom.cartesian_to_spherical(qs.T)
+
+        self.plot(mics, wavenumbers, data, En)
+
+    def locate_sources(self, *args, **kwargs):
+
+        super().locate_sources(*args, **kwargs)
+        self.colatitude_recon = self._doa_recon[0, :]
+        self.azimuth_recon = self._doa_recon[1, :]
+
+        # make azimuth always positive
+        I = self.azimuth_recon < 0.0
+        self.azimuth_recon[I] = 2.0 * np.pi + self.azimuth_recon[I]
+
+    def plot(self, mics, wavenumbers, data, En):
+        """
+        Plot the cost function for each cluster
+        """
+
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            import warnings
+
+            warnings.warn("Matplotlib is required for plotting")
+            return
+
+        grid = pra.doa.GridSphere(n_points=1000)
+
+        def func_cost(x, y, z):
+            qs = np.c_[x, y, z]
+            cost = []
+            for q in qs:
+                c = mmusic_cost(q, mics, wavenumbers, data, self.s)
+                cost.append(c)
+
+            return np.array(cost)
+
+        def func_cost2(x, y, z):
+            qs = np.c_[x, y, z]  # shape (n_grid, 3)
+
+            # conventional method
+            dt1 = qs @ self.L  # shape (n_grid, n_mics)
+            mv = np.exp(
+                2j * np.pi * self.freq_hz[None, :, None] * dt1[:, None, :] / self.c
+            )  # shape (n_grid, n_freq, n_mics)
+
+            ell1 = (
+                np.linalg.norm(
+                    mv[:, :, None, :] @ np.conj(En[None, :, :, :]), axis=(-2, -1)
+                )
+                ** 2
+            )  # shape (n_grid, n_freq)
+            cost1 = power_mean(ell1, s=self.s, axis=-1)
+
+            # new method
+            dt2 = qs @ self._L_diff  # shape (n_grid, n_mics)
+            e = 2 * np.pi * self.freq_hz[None, :, None] * dt2[:, None, :] / self.c
+            mvr = np.concatenate((np.cos(e), np.sin(e)), axis=-1,)
+
+            # shape (n_grid, n_freq, n_mics)
+
+            # shape (n_freq, n_mics)
+            EE = En @ np.conj(En).swapaxes(-2, -1)
+            v_c = self._extract_off_diagonal(0.5 * (EE + np.conj(EE.swapaxes(-2, -1))))
+            v_ri = np.concatenate((np.real(v_c), np.imag(v_c)), axis=-1)
+            diag_const = np.sum(np.linalg.norm(En, axis=-1) ** 2, axis=1)
+
+            ell2 = diag_const + 2 * np.sum(
+                v_ri[None, :, :] * mvr, axis=-1
+            )  # shape (n_grid, n_freq)
+
+            cost2 = power_mean(ell2, s=self.s, axis=-1)
+
+            return cost2
+
+        grid.apply(func_cost)
+
+        grid.plot(plotly=False)
